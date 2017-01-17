12992b77fcd36887e0b405c920afc9e8f85047eb
Mohsin Mohammad <M1037586@mindtree.com>
2017-01-17 16:10:33 +0530

    String push

--- src/org/mohsin/geek/String/PrintAllAngramsTogether.java
+++ src/org/mohsin/geek/String/PrintAllAngramsTogether.java
@@ -4,11 +4,23 @@ import java.util.Arrays;
 
 import org.mohsin.geek.List.ListNode;
 
+/*
+ *
+ * 
+ * Approach : 1. Iterate through every string in given array and sort it.
+ * 			  2. After sorting insert it into Trie and add index at leaf Node head.
+ * 			  3. Traverse trie and whenever there is a leaf, iterate through the list of index and print
+ *               the strings.
+ */
+
+
+// Trie Class
+
 class Anagramnode{
 	
-	boolean isLeaf;
-	Anagramnode children[];
-	ListNode head;
+	boolean isLeaf;                              //to store leaf information
+	Anagramnode children[];						 // to store child node
+	ListNode head;                              // to store indexes of strings
 	
 	public Anagramnode(){
 		isLeaf = false;
@@ -20,32 +32,37 @@ class Anagramnode{
 
 public class PrintAllAngramsTogether {
 
+	//function to insert into trie
+	
 	public static void insert(Anagramnode root,char str[],int val){
 		
-		Arrays.sort(str);
+		Arrays.sort(str);            // sort the string before insertion
 		
 		for(int i = 0;i < str.length;++i){
 			int index = str[i]-'a';
-			if(root.children[index] == null)
+			if(root.children[index] == null)  //if current node is not present, create a new one.
 				root.children[index] = new Anagramnode();
 			root = root.children[index];
 		}
 		
-		root.isLeaf = true;
+		root.isLeaf = true;              //mark it as leaf node
 		if(root.head == null){
-			root.head = new ListNode(val);
+			root.head = new ListNode(val);           // if head is null, initialize it and add index
 		}else{
-			ListNode newNode = new ListNode(val);
+			ListNode newNode = new ListNode(val);    // add index in the existing list
 			newNode.setNext(root.head);
 			root.head = newNode;
 		}
 	}
+	
+	//function to iterate through trie and print all anagrams together
 	
 	public static void print(Anagramnode root,String str[]){
 		
 		if(root == null)
 			return;
 		
+		//if its a leaf node iterate through list of index and print it.
 		if(root.isLeaf){
 			ListNode curr = root.head;
 			while(curr != null){
